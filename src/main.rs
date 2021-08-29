1c45d88c6566bc64c778d1382532c7a729ea8991
lizheng.lz1998 <lizheng.lz1998@bytedance.com>
2021-08-29 22:30:20 +0800

    update

--- src/main.rs
+++ src/main.rs
@@ -13,11 +13,11 @@ use axum::http::header::HeaderMap;
 use axum::response::{Html, IntoResponse};
 use axum::{AddExtensionLayer, Error};
 use axum::Router;
-use futures::{sink::SinkExt, stream::StreamExt};
+use futures::{sink::SinkExt, stream::StreamExt, Sink};
 use std::collections::{HashSet, HashMap};
 use std::net::SocketAddr;
 use std::sync::{Arc, Mutex};
-use tokio::sync::broadcast;
+use tokio::sync::{broadcast, oneshot};
 use headers::HeaderMapExt;
 use futures::stream::SplitSink;
 use tokio::sync::mpsc;
@@ -29,6 +29,9 @@ use prost::DecodeError;
 use rs_pbbot_demo::onebot::frame::{Data, FrameType};
 use rs_pbbot_demo::onebot::*;
 use rs_pbbot_demo::onebot;
+use tokio::sync::mpsc::Sender;
+use uuid::Uuid;
+use std::ops::Deref;
 
 // Our shared state
 struct AppState {
@@ -70,117 +73,203 @@ async fn websocket(stream: WebSocket, state: Arc<AppState>, bot_id: i64) {
     }
 
     let (mut ws_out, mut ws_in) = stream.split();
-    while let Some(msg) = ws_in.next().await {
-        match msg {
-            Ok(msg) => {
-                // println!("{:?}", msg);
-                match msg {
-                    Message::Text(_) => {}
-                    Message::Binary(buf) => {
-                        let frame: onebot::Frame = prost::Message::decode(&*buf).unwrap();
-                        match frame.data {
-                            None => { continue; }
-                            Some(data) => {
-                                match data {
-                                    Data::PrivateMessageEvent(event) => {
-                                        let out_frame = Frame {
-                                            bot_id,
-                                            frame_type: FrameType::TSendPrivateMsgReq.into(),
-                                            echo: "123".to_string(),
-                                            ok: true,
-                                            extra: Default::default(),
-                                            data: Some(Data::SendPrivateMsgReq(SendPrivateMsgReq {
-                                                user_id: event.user_id,
-                                                message: event.message,
-                                                auto_escape: false,
-                                            })),
-                                        };
-                                        let mut buf = Vec::new();
-                                        prost::Message::encode(&out_frame, &mut buf);
-                                        println!("{:?}", &buf);
-                                        if ws_out.send(Message::Binary(buf)).await.is_err() {
-                                            break;
+    let (api_sender, mut api_receiver) = mpsc::channel(10); // api channel
+    let resp_promises = Arc::new(Mutex::new(HashMap::new()));
+
+    // 发送 api req
+    let mut send_task = tokio::spawn(async move {
+        while let Some(frame) = api_receiver.recv().await {
+            let mut buf = Vec::new();
+            prost::Message::encode(&frame, &mut buf);
+            if ws_out.send(Message::Binary(buf)).await.is_err() {
+                break;
+            }
+        }
+    });
+
+    // 接受 event 和 api resp
+    let mut recv_task = tokio::spawn(async move {
+        while let Some(Ok(msg)) = ws_in.next().await {
+            match msg {
+                Message::Binary(buf) => {
+                    let frame: onebot::Frame = match prost::Message::decode(buf.as_ref()) {
+                        Ok(frame) => { frame }
+                        Err(_) => break
+                    };
+                    if let Some(data) = frame.clone().data {
+                        let mut bot = Bot { bot_id, api_sender: mpsc::Sender::clone(&api_sender), resp_promises: resp_promises.clone() };
+                        tokio::spawn(async move {
+                            match data {
+                                Data::PrivateMessageEvent(event) => {
+                                    let resp = bot.send_private_message(event.user_id, event.raw_message).await;
+                                    if let Some(resp) = resp {
+                                        println!("{}", resp.message_id)
+                                    }
+                                    if let Some(get_group_list_resp) = bot.get_group_list().await {
+                                        for group in get_group_list_resp.group {
+                                            println!("{} {}", group.group_id, group.group_name)
                                         }
-                                        println!("private message from {}: {}", event.user_id, event.raw_message);
                                     }
-                                    Data::GroupMessageEvent(event) => {
-                                        println!("group message from {}: {}", event.group_id, event.raw_message)
+                                }
+                                Data::GroupMessageEvent(event) => {}
+                                Data::GroupUploadNoticeEvent(event) => {}
+                                Data::GroupAdminNoticeEvent(event) => {}
+                                Data::GroupDecreaseNoticeEvent(event) => {}
+                                Data::GroupIncreaseNoticeEvent(event) => {}
+                                Data::GroupBanNoticeEvent(event) => {}
+                                Data::FriendAddNoticeEvent(event) => {}
+                                Data::GroupRecallNoticeEvent(event) => {}
+                                Data::FriendRecallNoticeEvent(event) => {}
+                                Data::FriendRequestEvent(event) => {}
+                                Data::GroupRequestEvent(event) => {}
+                                _ => {
+                                    // 不是 event，一定是 api resp
+                                    if let Some(api_resp_sender) = bot.resp_promises.lock().unwrap().remove(frame.echo.clone().as_str()) {
+                                        api_resp_sender.send(frame);
                                     }
-                                    Data::GroupUploadNoticeEvent(event) => {
-                                        println!("group file from {}: {}", event.group_id, event.file.unwrap().name)
-                                    }
-                                    Data::GroupAdminNoticeEvent(_) => {}
-                                    Data::GroupDecreaseNoticeEvent(_) => {}
-                                    Data::GroupIncreaseNoticeEvent(_) => {}
-                                    Data::GroupBanNoticeEvent(_) => {}
-                                    Data::FriendAddNoticeEvent(_) => {}
-                                    Data::GroupRecallNoticeEvent(_) => {}
-                                    Data::FriendRecallNoticeEvent(_) => {}
-                                    Data::FriendRequestEvent(_) => {}
-                                    Data::GroupRequestEvent(_) => {}
-                                    _ => {}
                                 }
                             }
-                        }
+                        });
                     }
-                    Message::Ping(_) => {}
-                    Message::Close(_) => { break; }
-                    _ => {}
                 }
+                Message::Close(_) => { break; }
+                _ => {}
             }
-            Err(err) => { break; }
         }
+    });
+
+    tokio::select! {
+        _ = (&mut send_task) => recv_task.abort(),
+        _ = (&mut recv_task) => send_task.abort(),
     }
-    state.bots.lock().unwrap().remove(&bot_id); // 掉线就会break while
-
-
-    // let (sender, mut receiver) = mpsc::channel(100);
-    //
-    // let bot = Bot { bot_id, sender: Mutex::new(sender) };
-
-
-    // let mut send_task = tokio::spawn(async move {
-    //     while let Some(frame) = receiver.recv().await {
-    //         //     if ws_out.send(msg).await.is_err() {
-    //         //         break;
-    //         //     }
-    //     }
-    // });
-    //
-    // // Clone things we want to pass to the receiving task.
-    //
-    // // This task will receive messages from client and send them to broadcast subscribers.
-    // let mut recv_task = tokio::spawn(async move {
-    //     while let Some(Ok(Message::Binary(buf))) = ws_in.next().await {
-    //         let frame: onebot::Frame = match prost::Message::decode(&*buf) {
-    //             Ok(frame) => { frame }
-    //             Err(_) => { continue; }
-    //         };
-    //         let a = frame;
-    //     }
-    // });
-
-    // // If any one of the tasks exit, abort the other.
-    // tokio::select! {
-    //     _ = (&mut send_task) => recv_task.abort(),
-    //     _ = (&mut recv_task) => send_task.abort(),
-    // };
-
-
-    // Send user left message.
-
-    // Remove username from map so new clients can take it.
+    ;
 }
 
 
 struct Bot {
     bot_id: i64,
-    sender: Mutex<mpsc::Sender<onebot::Frame>>,
-
+    api_sender: Sender<onebot::Frame>,
+    resp_promises: Arc<Mutex<HashMap<String, oneshot::Sender<onebot::Frame>>>>,
 }
 
 impl Bot {
     pub fn on_frame(&self) {}
-    pub fn send_and_wait() {}
+    pub async fn send_and_wait(&mut self, data: Data) -> Option<Data> {
+        // 构造API请求
+        let echo: String = uuid::Uuid::new_v4().to_simple().to_string();
+        let req_frame_type = get_frame_type(&data).into();
+        let api_req_frame = Frame {
+            bot_id: self.bot_id,
+            frame_type: req_frame_type,
+            echo: echo.clone(),
+            ok: true,
+            extra: Default::default(),
+            data: Some(data),
+        };
+
+        // 发送API请求
+        let mut buf = Vec::new();
+        prost::Message::encode(&api_req_frame, &mut buf);
+        let api_sender = mpsc::Sender::clone(&self.api_sender);
+        api_sender.send(api_req_frame).await;
+
+        // 等待API响应
+        let (mut resp_sender, mut resp_receiver) = oneshot::channel();
+        self.resp_promises.lock().unwrap().insert(echo.clone(), resp_sender);
+        let api_resp_frame = resp_receiver.await.unwrap();
+        return api_resp_frame.data;
+    }
+    pub async fn send_private_message(&mut self, user_id: i64, content: String) -> Option<SendPrivateMsgResp> {
+        let resp = self.send_and_wait(Data::SendPrivateMsgReq(SendPrivateMsgReq {
+            user_id,
+            message: vec![
+                onebot::Message {
+                    r#type: "text".to_string(),
+                    data: [
+                        ("text".to_string(), content),
+                    ].iter().cloned().collect(),
+                }
+            ],
+            auto_escape: false,
+        })).await;
+        if let Some(Data::SendPrivateMsgResp(resp)) = resp {
+            Some(resp)
+        } else {
+            None
+        }
+    }
+    pub async fn send_group_message(&mut self, group_id: i64, content: String) -> Option<SendGroupMsgResp> {
+        let resp = self.send_and_wait(Data::SendGroupMsgReq(SendGroupMsgReq {
+            group_id,
+            message: vec![
+                onebot::Message {
+                    r#type: "text".to_string(),
+                    data: [
+                        ("text".to_string(), content),
+                    ].iter().cloned().collect(),
+                }
+            ],
+            auto_escape: false,
+        })).await;
+        if let Some(Data::SendGroupMsgResp(resp)) = resp {
+            Some(resp)
+        } else {
+            None
+        }
+    }
+    pub async fn get_group_list(&mut self) -> Option<GetGroupListResp> {
+        let resp = self.send_and_wait(Data::GetGroupListReq(GetGroupListReq {})).await;
+        if let Some(Data::GetGroupListResp(resp)) = resp {
+            Some(resp)
+        } else {
+            None
+        }
+    }
 }
+
+
+fn get_frame_type(data: &Data) -> onebot::frame::FrameType {
+    match data {
+        Data::SendPrivateMsgReq(_) => { FrameType::TSendPrivateMsgReq }
+        Data::SendGroupMsgReq(_) => { FrameType::TSendGroupMsgReq }
+        Data::SendMsgReq(_) => { FrameType::TSendGroupMsgReq }
+        Data::DeleteMsgReq(_) => { FrameType::TDeleteMsgReq }
+        Data::GetMsgReq(_) => { FrameType::TGetMsgReq }
+        Data::GetForwardMsgReq(_) => { FrameType::TGetForwardMsgReq }
+        Data::SendLikeReq(_) => { FrameType::TSendLikeReq }
+        Data::SetGroupKickReq(_) => { FrameType::TSetGroupKickReq }
+        Data::SetGroupBanReq(_) => { FrameType::TSetGroupBanReq }
+        Data::SetGroupAnonymousBanReq(_) => { FrameType::TSetGroupAnonymousBanReq }
+        Data::SetGroupWholeBanReq(_) => { FrameType::TSetGroupWholeBanReq }
+        Data::SetGroupAdminReq(_) => { FrameType::TSetGroupAdminReq }
+        Data::SetGroupAnonymousReq(_) => { FrameType::TSetGroupAnonymousReq }
+        Data::SetGroupCardReq(_) => { FrameType::TSetGroupCardReq }
+        Data::SetGroupNameReq(_) => { FrameType::TSetGroupNameReq }
+        Data::SetGroupLeaveReq(_) => { FrameType::TSetGroupLeaveReq }
+        Data::SetGroupSpecialTitleReq(_) => { FrameType::TSetGroupSpecialTitleReq }
+        Data::SetFriendAddRequestReq(_) => { FrameType::TSetFriendAddRequestReq }
+        Data::SetGroupAddRequestReq(_) => { FrameType::TSetGroupAddRequestReq }
+        Data::GetLoginInfoReq(_) => { FrameType::TGetLoginInfoReq }
+        Data::GetStrangerInfoReq(_) => { FrameType::TGetStrangerInfoReq }
+        Data::GetFriendListReq(_) => { FrameType::TGetFriendListReq }
+        Data::GetGroupInfoReq(_) => { FrameType::TGetGroupInfoReq }
+        Data::GetGroupListReq(_) => { FrameType::TGetGroupListReq }
+        Data::GetGroupMemberInfoReq(_) => { FrameType::TGetGroupMemberInfoReq }
+        Data::GetGroupMemberListReq(_) => { FrameType::TGetGroupMemberListReq }
+        Data::GetGroupHonorInfoReq(_) => { FrameType::TGetGroupHonorInfoReq }
+        Data::GetCookiesReq(_) => { FrameType::TGetCookiesReq }
+        Data::GetCsrfTokenReq(_) => { FrameType::TGetCsrfTokenReq }
+        Data::GetCredentialsReq(_) => { FrameType::TGetCredentialsReq }
+        Data::GetRecordReq(_) => { FrameType::TGetRecordReq }
+        Data::GetImageReq(_) => { FrameType::TGetImageReq }
+        Data::CanSendImageReq(_) => { FrameType::TCanSendImageReq }
+        Data::CanSendRecordReq(_) => { FrameType::TCanSendRecordReq }
+        Data::GetStatusReq(_) => { FrameType::TGetStatusReq }
+        Data::GetVersionInfoReq(_) => { FrameType::TGetVersionInfoReq }
+        Data::SetRestartReq(_) => { FrameType::TSetRestartReq }
+        Data::CleanCacheReq(_) => { FrameType::TCleanCacheReq }
+        _ => { FrameType::Tunknown }
+    }
+}
+
 
