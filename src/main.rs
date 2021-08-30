c58925b7d37fbea73c7193fdfd4a60da6842d4e2
NKDark <nkdark214@gmail.com>
2021-08-30 15:11:37 +0800

    修改文档注释格式

--- src/main.rs
+++ src/main.rs
@@ -184,13 +184,13 @@ impl Bot {
         return api_resp_frame.data;
     }
     
-    /**
-     * 发送私聊消息
-     *
-     * @param user_id          对方 QQ 号
-     * @param content          消息内容
-     * @return 结果
-     */
+    ///
+    /// 发送私聊消息
+    ///
+    /// @param user_id          对方 QQ 号
+    /// @param content          消息内容
+    /// @return 结果
+    ///
     pub async fn send_private_message(&mut self, user_id: i64, content: String) -> Option<SendPrivateMsgResp> {
         let resp = self.send_and_wait(Data::SendPrivateMsgReq(SendPrivateMsgReq {
             user_id,
@@ -211,13 +211,13 @@ impl Bot {
         }
     }
     
-    /**
-     * 发送群消息
-     *
-     * @param group_id         群号
-     * @param content          消息
-     * @return 结果
-     */
+    ///
+    /// 发送群消息
+    ///
+    /// @param group_id         群号
+    /// @param content          消息
+    /// @return 结果
+    ///
     pub async fn send_group_message(&mut self, group_id: i64, content: String) -> Option<SendGroupMsgResp> {
         let resp = self.send_and_wait(Data::SendGroupMsgReq(SendGroupMsgReq {
             group_id,
@@ -238,12 +238,12 @@ impl Bot {
         }
     }
     
-    /**
-     * 撤回消息
-     *
-     * @param message_id 消息 ID
-     * @return 结果
-     */
+    ///
+    /// 撤回消息
+    ///
+    /// @param message_id 消息 ID
+    /// @return 结果
+    ///
     pub async fn delete_msg(&mut self, message_id: i32) -> Option<DeleteMsgResp> {
         let resp = self.send_and_wait(Data::DeleteMsgReq(DeleteMsgReq {
             message_id
@@ -255,12 +255,12 @@ impl Bot {
         }
     }
     
-    /**
-     * 获取消息
-     *
-     * @param message_id 消息 ID
-     * @return 结果
-     */
+   ///
+   /// 获取消息
+   ///
+   /// @param message_id 消息 ID
+   /// @return 结果
+   ///
     pub async fn get_msg(&mut self, message_id: i32) -> Option<GetMsgResp> {
         let resp = self.send_and_wait(Data::GetMsgReq(GetMsgReq {
             message_id
@@ -272,14 +272,14 @@ impl Bot {
         }
     }
 
-    /**
-     * 群组踢人
-     *
-     * @param group_id           群号
-     * @param user_id            要踢的 QQ 号
-     * @param reject_add_request 拒绝此人的加群请求
-     * @return 结果
-     */
+    ///
+    /// 群组踢人
+    ///
+    /// @param group_id           群号
+    /// @param user_id            要踢的 QQ 号
+    /// @param reject_add_request 拒绝此人的加群请求
+    /// @return 结果
+    ///
     pub async fn set_group_kick(&mut self, group_id: i64, user_id: i64, reject_add_request: bool) -> Option<SetGroupKickResp> {
         let resp = self.send_and_wait(Data::SetGroupKickReq(SetGroupKickReq {
             group_id,
@@ -293,14 +293,14 @@ impl Bot {
         }
     }
 
-    /**
-     * 群组单人禁言
-     *
-     * @param group_id 群号
-     * @param user_id  要禁言的 QQ 号
-     * @param duration 禁言时长，单位秒，0 表示取消禁言
-     * @return 结果
-     */
+    ///
+    /// 群组单人禁言
+    ///
+    /// @param group_id 群号
+    /// @param user_id  要禁言的 QQ 号
+    /// @param duration 禁言时长，单位秒，0 表示取消禁言
+    /// @return 结果
+    ///
     pub async fn set_group_ban(&mut self, group_id: i64, user_id: i64, duration: i32) -> Option<SetGroupBanResp> {
         let resp = self.send_and_wait(Data::SetGroupBanReq(SetGroupBanReq {
             group_id,
@@ -314,13 +314,13 @@ impl Bot {
         }
     }
 
-    /**
-     * 群组全员禁言
-     *
-     * @param group_id 群号
-     * @param enable   是否禁言
-     * @return 结果
-     */
+    ///
+    /// 群组全员禁言
+    ///
+    /// @param group_id 群号
+    /// @param enable   是否禁言
+    /// @return 结果
+    ///
     pub async fn set_group_whole_ban(&mut self, group_id: i64, enable: bool) -> Option<SetGroupWholeBanResp> {
         let resp = self.send_and_wait(Data::SetGroupWholeBanReq(SetGroupWholeBanReq {
             group_id,
@@ -333,14 +333,14 @@ impl Bot {
         }
     }
 
-    /**
-     * 设置群名片（群备注）
-     *
-     * @param group_id 群号
-     * @param user_id  要设置的 QQ 号
-     * @param card     群名片内容，空字符串表示删除群名片
-     * @return 结果
-     */
+    ///
+    /// 设置群名片（群备注）
+    ///
+    /// @param group_id 群号
+    /// @param user_id  要设置的 QQ 号
+    /// @param card     群名片内容，空字符串表示删除群名片
+    /// @return 结果
+    ///
     pub async fn set_group_card(&mut self, group_id: i64, user_id: i64, card: String) -> Option<SetGroupCardResp> {
         let resp = self.send_and_wait(Data::SetGroupCardReq(SetGroupCardReq {
             group_id,
@@ -354,13 +354,13 @@ impl Bot {
         }
     }
 
-    /**
-     * 解散群组
-     *
-     * @param group_id   群号
-     * @param is_dismiss 是否解散，如果登录号是群主，则仅在此项为 true 时能够解散
-     * @return 结果
-     */
+    ///
+    /// 解散群组
+    ///
+    /// @param group_id   群号
+    /// @param is_dismiss 是否解散，如果登录号是群主，则仅在此项为 true 时能够解散
+    /// @return 结果
+    ///
     pub async fn set_group_leave(&mut self, group_id: i64, is_dismiss: bool) -> Option<SetGroupLeaveResp> {
         let resp = self.send_and_wait(Data::SetGroupLeaveReq(SetGroupLeaveReq {
             group_id,
@@ -373,15 +373,15 @@ impl Bot {
         }
     }
 
-    /**
-     * 设置群组专属头衔
-     *
-     * @param group_id      群号
-     * @param user_id       要设置的 QQ 号
-     * @param special_title 专属头衔，不填或空字符串表示删除专属头衔
-     * @param duration      专属头衔有效期，单位秒，-1 表示永久，不过此项似乎没有效果，可能是只有某些特殊的时间长度有效，有待测试
-     * @return 结果
-     */
+    ///
+    /// 设置群组专属头衔
+    ///
+    /// @param group_id      群号
+    /// @param user_id       要设置的 QQ 号
+    /// @param special_title 专属头衔，不填或空字符串表示删除专属头衔
+    /// @param duration      专属头衔有效期，单位秒，-1 表示永久，不过此项似乎没有效果，可能是只有某些特殊的时间长度有效，有待测试
+    /// @return 结果
+    ///
     pub async fn set_group_special_title(&mut self, group_id: i64, user_id: i64, special_title: String, duration: i64) -> Option<SetGroupSpecialTitleResp> {
         let resp = self.send_and_wait(Data::SetGroupSpecialTitleReq(SetGroupSpecialTitleReq {
             group_id,
@@ -396,14 +396,14 @@ impl Bot {
         }
     }
 
-    /**
-     * 处理加好友请求
-     *
-     * @param flag    加好友请求的 flag（需从上报的数据中获得）
-     * @param approve 是否同意请求
-     * @param remark  添加后的好友备注（仅在同意时有效）
-     * @return 结果
-     */
+    ///
+    /// 处理加好友请求
+    ///
+    /// @param flag    加好友请求的 flag（需从上报的数据中获得）
+    /// @param approve 是否同意请求
+    /// @param remark  添加后的好友备注（仅在同意时有效）
+    /// @return 结果
+    ///
     pub async fn set_friend_add_request(&mut self, flag: String, approve: bool, remark: String) -> Option<SetFriendAddRequestResp> {
         let resp = self.send_and_wait(Data::SetFriendAddRequestReq(SetFriendAddRequestReq {
             flag,
@@ -417,16 +417,16 @@ impl Bot {
         }
     }
 
-    /**
-     * 处理加群请求／邀请
-     *
-     * @param flag     加群请求的 flag（需从上报的数据中获得）
-     * @param sub_type add 或 invite，请求类型（需要和上报消息中的 sub_type 字段相符）
-     * @param approve  是否同意请求／邀请
-     * @param reason   拒绝理由（仅在拒绝时有效）
-     * @return 结果
-     */
-    // TODO
+    ///
+    /// 处理加群请求／邀请
+    ///
+    /// @param flag     加群请求的 flag（需从上报的数据中获得）
+    /// @param sub_type add 或 invite，请求类型（需要和上报消息中的 sub_type 字段相符）
+    /// @param approve  是否同意请求／邀请
+    /// @param reason   拒绝理由（仅在拒绝时有效）
+    /// @return 结果
+    ///
+    // TODO r#type 不知道怎么写
     pub async fn set_group_add_request(&mut self, flag: String,sub_type: String, approve: bool, reason: String) -> Option<SetGroupAddRequestResp> {
         let resp = self.send_and_wait(Data::SetGroupAddRequestReq(SetGroupAddRequestReq {
             flag,
@@ -442,11 +442,11 @@ impl Bot {
         }
     }
 
-    /**
-     * 获取登录号信息
-     *
-     * @return 结果
-     */
+    ///
+    /// 获取登录号信息
+    ///
+    /// @return 结果
+    ///
     pub async fn get_login_info(&mut self) -> Option<GetLoginInfoResp> {
         let resp = self.send_and_wait(Data::GetLoginInfoReq(GetLoginInfoReq {})).await;
         if let Some(Data::GetLoginInfoResp(resp)) = resp {
@@ -456,12 +456,12 @@ impl Bot {
         }
     }
 
-    /**
-     * 获取陌生人信息
-     *
-     * @param user_id   QQ号
-     * @return 结果
-     */
+    ///
+    /// 获取陌生人信息
+    ///
+    /// @param user_id   QQ号
+    /// @return 结果
+    ///
     pub async fn get_stranger_info(&mut self, user_id: i64) -> Option<GetStrangerInfoResp> {
         let resp = self.send_and_wait(Data::GetStrangerInfoReq(GetStrangerInfoReq {
             user_id,
@@ -474,11 +474,11 @@ impl Bot {
         }
     }
 
-    /**
-     * 获取好友列表
-     *
-     * @return 结果
-     */
+    ///
+    /// 获取好友列表
+    ///
+    /// @return 结果
+    ///
     pub async fn get_friend_list(&mut self) -> Option<GetFriendListResp> {
         let resp = self.send_and_wait(Data::GetFriendListReq(GetFriendListReq {})).await;
         if let Some(Data::GetFriendListResp(resp)) = resp {
@@ -488,11 +488,11 @@ impl Bot {
         }
     }
 
-    /**
-     * 获取群列表
-     *
-     * @return 结果
-     */
+    ///
+    /// 获取群列表
+    ///
+    /// @return 结果
+    ///
     pub async fn get_group_list(&mut self) -> Option<GetGroupListResp> {
         let resp = self.send_and_wait(Data::GetGroupListReq(GetGroupListReq {})).await;
         if let Some(Data::GetGroupListResp(resp)) = resp {
@@ -502,13 +502,13 @@ impl Bot {
         }
     }
 
-    /**
-     * 获取群信息
-     *
-     * @param group_id 群号
-     * @param no_cache 是否不使用缓存（使用缓存可能更新不及时，但响应更快）
-     * @return 结果
-     */
+    ///
+    /// 获取群信息
+    ///
+    /// @param group_id 群号
+    /// @param no_cache 是否不使用缓存（使用缓存可能更新不及时，但响应更快）
+    /// @return 结果
+    ///
     pub async fn get_group_info(&mut self, group_id: i64, no_cache: bool) -> Option<GetGroupInfoResp> {
         let resp = self.send_and_wait(Data::GetGroupInfoReq(GetGroupInfoReq{
             group_id,
@@ -521,14 +521,14 @@ impl Bot {
         }
     }
 
-    /**
-     * 获取群成员信息
-     *
-     * @param group_id 群号
-     * @param user_id  QQ 号
-     * @param no_cache 是否不使用缓存（使用缓存可能更新不及时，但响应更快）
-     * @return 结果
-     */
+    ///
+    /// 获取群成员信息
+    ///
+    /// @param group_id 群号
+    /// @param user_id  QQ 号
+    /// @param no_cache 是否不使用缓存（使用缓存可能更新不及时，但响应更快）
+    /// @return 结果
+    ///
     pub async fn get_group_member_info(&mut self,group_id: i64, user_id: i64, no_cache: bool) -> Option<GetGroupMemberInfoResp> {
         let resp = self.send_and_wait(Data::GetGroupMemberInfoReq(GetGroupMemberInfoReq {
             group_id,
@@ -542,15 +542,15 @@ impl Bot {
         }
     }
 
-    /**
-     * 获取群成员列表
-     *
-     *
-     * 响应内容为 JSON 数组，每个元素的内容和上面的 /get_group_member_info 接口相同，但对于同一个群组的同一个成员，获取列表时和获取单独的成员信息时，某些字段可能有所不同，例如 area、title 等字段在获取列表时无法获得，具体应以单独的成员信息为准。
-     *
-     * @param group_id 群号
-     * @return 结果
-     */
+    ///
+    /// 获取群成员列表
+    ///
+    ///
+    /// 响应内容为 JSON 数组，每个元素的内容和上面的 /get_group_member_info 接口相同，但对于同一个群组的同一个成员，获取列表时和获取单独的成员信息时，某些字段可能有所不同，例如 area、title 等字段在获取列表时无法获得，具体应以单独的成员信息为准。
+    ///
+    /// @param group_id 群号
+    /// @return 结果
+    ///
     pub async fn get_group_member_list(&mut self, group_id: i64) -> Option<GetGroupListResp> {
         let resp = self.send_and_wait(Data::GetGroupListReq(GetGroupListReq {})).await;
         if let Some(Data::GetGroupListResp(resp)) = resp {
